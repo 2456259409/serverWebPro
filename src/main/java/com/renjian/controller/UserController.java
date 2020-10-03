810739b0be00f1bd2d150206a110aaf1b5022332
Renjian <2456259409@qq.com>
2020-10-03 19:38:51 +0800

    修改个人信息

--- src/main/java/com/renjian/controller/UserController.java
+++ src/main/java/com/renjian/controller/UserController.java
@@ -2,6 +2,7 @@ package com.renjian.controller;
 
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.crypto.SecureUtil;
+import cn.hutool.json.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.renjian.model.KeyWord;
 import com.renjian.model.User;
@@ -10,6 +11,8 @@ import com.renjian.utils.CommonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/api/user")
@@ -55,6 +58,23 @@ public class UserController {
             return new CommonResult().success("注册成功");
         }
         return new CommonResult().failed("用户名已经存在");
+    }
+
+    @PostMapping("/updateUser")
+    public Object updateUser(User user){
+        QueryWrapper<User> wrapper=new QueryWrapper<>();
+        wrapper.eq("id",user.getId());
+        User one = userService.getOne(wrapper);
+        if(one!=null){
+            if(user.getPassword()!=null){
+                String s=resoleSalt.replace("T",user.getSalt()+user.getPassword());
+                String pass = SecureUtil.md5(s);
+                user.setPassword(pass);
+            }
+            userService.updateById(user);
+            return new CommonResult().success("修改成功");
+        }
+        return new CommonResult().failed("用户不存在");
     }
 
     @PostMapping("/search")
