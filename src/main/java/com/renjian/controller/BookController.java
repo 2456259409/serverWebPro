0bbec57aab0a0d8aac26ecb344fcf0246c477c70
Renjian <2456259409@qq.com>
2020-11-11 21:56:02 +0800

    添加图书

--- src/main/java/com/renjian/controller/BookController.java
+++ src/main/java/com/renjian/controller/BookController.java
@@ -1,16 +1,52 @@
 package com.renjian.controller;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.renjian.model.Book;
+import com.renjian.service.BookService;
 import com.renjian.utils.CommonResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/book")
 @CrossOrigin
 public class BookController {
 
+    @Resource
+    BookService bookService;
+
     @GetMapping("/get_all")
     public Object getAllBook(){
 
         return new CommonResult().success("ok123");
+    }
+
+    @PostMapping("/add_book")
+    public Object addABook(@RequestBody JSONObject jsonObject){
+
+        Book book=JSONUtil.toBean(jsonObject, Book.class);
+        if(book==null){
+            return new CommonResult().failed("请传入书籍");
+        }
+        if(bookService.getOne(new QueryWrapper<Book>().eq("creator_id",book.getCreatorId()).eq("author",book.getAuthor()).eq("book_name",book.getBookName()))!=null){
+            return new CommonResult().failed("该书籍已经在库中");
+        }
+        book.setStatus(1);
+        book.setCreateTime(new Date());
+        book.setTypesToStr();
+        if(book.getBookNumber()==null){
+            book.setBookNumber(RandomUtil.randomNumbers(15));
+        }
+        boolean b = bookService.saveOrUpdate(book);
+        if(b){
+            return new CommonResult().success("添加成功");
+        }else{
+            return new CommonResult().failed("保存失败!!!");
+        }
     }
 }
--- src/main/java/com/renjian/model/Book.java
+++ src/main/java/com/renjian/model/Book.java
@@ -1,6 +1,9 @@
 package com.renjian.model;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -12,13 +15,45 @@ import java.util.Date;
 public class Book {
     @TableId(type = IdType.AUTO,value = "id")
     private Long id;
+    private Long creatorId;
     private String author;
     private Integer count;
+    private String bookName;
     private Date createTime;
     /**
      * 0已经被删除；1待审核；2审核通过
      */
     private Integer status;
+
+    /**
+     * 创建者是否推荐
+     */
+    private Integer creatorRecommond;
+    /**
+     * 管理员是否推荐
+     */
+    private Integer mannagerRecommond;
     private String bookNumber;
     private String introduce;
+
+    private String strTypes;
+
+    private Date deadline;
+
+    public void setDeadLineTime(){
+        deadline= DateUtil.parse(deadDay+" "+deadTime);
+    }
+
+    public void setTypesToStr(){
+        strTypes= JSONUtil.toJsonStr(types);
+    }
+
+    @TableField(exist = false)
+    private String deadDay;
+
+    @TableField(exist = false)
+    private String deadTime;
+
+    @TableField(exist = false)
+    private Long[] types;
 }
