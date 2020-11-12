518322e5218186f06b44069cb453aaec1970104a
Renjian <2456259409@qq.com>
2020-11-12 21:45:29 +0800

    我的图书部分

--- src/main/java/com/renjian/controller/BookController.java
+++ src/main/java/com/renjian/controller/BookController.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.renjian.mapper.BookMapper;
 import com.renjian.model.Book;
 import com.renjian.service.BookService;
 import com.renjian.utils.CommonResult;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
@@ -19,6 +21,9 @@ public class BookController {
 
     @Resource
     BookService bookService;
+
+    @Resource
+    BookMapper bookMapper;
 
     @GetMapping("/get_all")
     public Object getAllBook(){
@@ -47,6 +52,63 @@ public class BookController {
             return new CommonResult().success("添加成功");
         }else{
             return new CommonResult().failed("保存失败!!!");
+        }
+    }
+
+    @GetMapping("/get_books")
+    public Object getBooks(int keyword,Long userId,int pageNum,int pageSize){
+        QueryWrapper<Book> wrapper=new QueryWrapper<>();
+        wrapper.eq("creator_id",userId);
+        if(keyword==1){
+            wrapper.in("status",1,2);
+        }else if(keyword==2){
+            wrapper.eq("status",1);
+        }else if(keyword==3){
+            wrapper.eq("status",2);
+        }
+
+        StringBuffer sb=new StringBuffer();
+        sb.append("limit ");
+        sb.append(pageNum*pageSize);
+        sb.append(",");
+        sb.append((pageNum+1)*pageSize);
+        wrapper.last(sb.toString());
+        List<Book> list = bookService.list(wrapper);
+        return new CommonResult().success(list);
+    }
+
+    @GetMapping("/delete_book")
+    public Object deleteBook(Long id){
+
+        boolean b = bookMapper.updateBookStatus(id, 0);
+        if(b){
+            return new CommonResult().success("删除成功");
+        }else{
+            return new CommonResult().failed("删除失败");
+        }
+    }
+
+    @PostMapping("/update_book")
+    public Object updateBook(@RequestBody JSONObject json){
+        Book book = JSONUtil.toBean(json, Book.class);
+        boolean b = bookService.saveOrUpdate(book);
+        if(b){
+            return new CommonResult().success("修改成功");
+        }else{
+            return new CommonResult().failed("修改失败");
+        }
+    }
+
+    @GetMapping("/through_book")
+    public Object throughBook(Long id,Long userId){
+        if(userId!=10){
+            return new CommonResult().failed("您无权审核该图书");
+        }
+        boolean b = bookMapper.updateBookStatus(id, 2);
+        if(b){
+            return new CommonResult().success("审核成功");
+        }else{
+            return new CommonResult().failed("审核失败");
         }
     }
 }
--- src/main/java/com/renjian/mapper/BookMapper.java
+++ src/main/java/com/renjian/mapper/BookMapper.java
@@ -3,7 +3,10 @@ package com.renjian.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.renjian.model.Book;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface BookMapper extends BaseMapper<Book> {
+    @Update("update rj_book set status=#{status} where id=#{id}")
+    boolean updateBookStatus(Long id,Integer status);
 }
